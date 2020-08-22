bdbe2e1de7e911ea53457c66c828187bc30397ab
Robert Pelz <pelz.robert@gmail.com>
2020-08-22 21:03:10 +0200

    Initial Commit

---
+++ CoVStats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoVStats
+//
+//  Created by Robert Pelz on 22.08.20.
+//  Copyright © 2020 Robert Pelz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
